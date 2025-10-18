52fdc3dc3b22a3366534e73c29826b2aa48c451e
leaodebrito <leaodebrito@icloud.com>
2025-10-18 10:27:09 -0300

    Inicio de desenvolvimento - adição de sdk do firebase

---
+++ sistema_bup/sistema_bup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sistema_bup
+//
+//  Created by Bruno Brito on 18/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sistema_bup/sistema_bup/sistema_bupApp.swift
@@ -0,0 +1,17 @@
+//
+//  sistema_bupApp.swift
+//  sistema_bup
+//
+//  Created by Bruno Brito on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct sistema_bupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
